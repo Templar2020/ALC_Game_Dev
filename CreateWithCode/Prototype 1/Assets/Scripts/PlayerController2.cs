c9f2344cf05023fdf5e7837e1402cffc952bfd77
Mr. Powell <preston.powell@nebo.edu>
2019-09-17 10:33:11 -0600

    Updated player controller. Vehicle rotates instead of slides.

--- CreateWithCode/Prototype 1/Assets/Scripts/PlayerController2.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController2.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
-    public float speed = 20f; 
+    public float speed = 20f;
+
+    public float turnSpeed = 5f;
+
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,10 @@ public class PlayerController2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
