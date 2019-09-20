ae2d7a049f17cc8a107e91c7418fc411d1f4750e
Mr. Powell <preston.powell@nebo.edu>
2019-09-20 15:10:14 -0600

    Game Update

--- CreateWithCode/Prototype 1/Assets/Scripts/PlayerController2.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController2.cs
@@ -4,28 +4,18 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
-    public float speed = 20f;
-
-    public float turnSpeed = 5f;
-
-    public float horizontalInput;
-
-    public float forwardInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Player control private variables
+    private float speed = 20f;
+    private float turnSpeed = 15f;
+    private float horizontalInput;
+    private float forwardInput;    
 
     // Update is called once per frame
     void Update()
     {
+        // This codemoves the vehicle foward and back. Rotates it left and right. 
         horizontalInput = Input.GetAxis("Horizontal");
-
-        forwardInput = Input.GetAxis("Vertical");
-
-        // We'll move the vehicle forward
+        forwardInput = Input.GetAxis("Vertical");        
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
         
